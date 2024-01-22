9baf0956ecee7556343ce085c3067388969e2e5c
Eun6636 <tktmdmswl004@gmail.com>
2024-01-22 21:38:53 +0900

    UI

---
+++ Assets/Script/Entities/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChararcterType
+{
+    Penfuin,
+    BearJelly
+}
+
+public class Character
+{
+    public ChararcterType chararcterType;
+    public Transform transform;
+    public Sprite sprite;
+    public RuntimeAnimatorController Animator;
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform
+    void Awake()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/UI/PopupStartMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupStartMenu : MonoBehaviour
+{
+    [SerializeField] private InputField InputField;
+    [SerializeField] private Text playerName;
+
+    public void OnCleakJoin()
+    {
+        if(!(2 < InputField.text.Length && InputField.text.Length <10))
+        {
+            return;
+        }
+
+        playerName.text = InputField.text;
+        Destroy(gameObject);
+    }
+}
