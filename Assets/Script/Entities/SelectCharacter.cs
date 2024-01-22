ae1645a65691a9ae1b2c5536b9a1b126ba6e4a02
Eun6636 <tktmdmswl004@gmail.com>
2024-01-22 19:29:03 +0900

    맵 배치

---
+++ Assets/Script/Entities/SelectCharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharacter : MonoBehaviour
+{
+    [SerializeField] private Transform transform;
+    [SerializeField] private SpriteRenderer CharSprite;
+    [SerializeField] private Animator animator;
+}
