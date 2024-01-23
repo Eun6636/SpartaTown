f356135c2f9af7dff3d637bb1495208646df72c9
Eun6636 <tktmdmswl004@gmail.com>
2024-01-23 12:09:01 +0900

    캐릭터 선택

--- Assets/Script/Entities/GameManager.cs
+++ Assets/Script/Entities/GameManager.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum ChararcterType
 {
-    Penfuin,
+    Penfuin ,
     BearJelly
 }
 
+
+[System.Serializable]
 public class Character
 {
-    public ChararcterType chararcterType;
-    public Transform transform;
-    public Sprite sprite;
+    public ChararcterType Chararctertype;
+    public Sprite CharSprite;
     public RuntimeAnimatorController Animator;
 }
 
-//public class GameManager : MonoBehaviour
-//{
-//    [SerializeField] private Transform
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
 
-//}
+    public Animator PlayerAnimator;
+    public Text playerName;
+    public SpriteRenderer Playersprite;
+    public Transform playerTransform;
+
+    public List<Character> charactList = new List<Character>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void SetCharacter(ChararcterType Type, string name,Sprite sprite)
+    {
+        var Char = GameManager.Instance.charactList.Find(item => item.Chararctertype == Type);
+
+        if (Char.Chararctertype == ChararcterType.BearJelly)
+        {
+            playerTransform.position = new Vector3(playerTransform.position.x,
+                playerTransform.position.y- 1.6f, 0);
+        }
+
+        PlayerAnimator.runtimeAnimatorController = Char.Animator;
+        Playersprite.sprite = sprite;
+        playerName.text = name;
+    }
+
+}
--- Assets/Script/UI/PopupStartMenu.cs
+++ Assets/Script/UI/PopupStartMenu.cs
@@ -5,18 +5,40 @@ using UnityEngine.UI;
 
 public class PopupStartMenu : MonoBehaviour
 {
-    public Text playerName;
+
+    [SerializeField] private Image CharacterSprite;
     [SerializeField] private InputField InputField;
+    [SerializeField] private GameObject Information;
+    [SerializeField] private GameObject SelectCharacter;
+
+    private ChararcterType chararcterType;
+
+    public void OncleakCharacter()
+    {
+        Information.SetActive(false);
+        SelectCharacter.SetActive(true);
+    }
+
+    public void OnCleakSelectCharecter(int index)
+    {
+        chararcterType = (ChararcterType)index;
+        var Char = GameManager.Instance.charactList.Find(item =>item.Chararctertype == chararcterType);
+        CharacterSprite.sprite = Char.CharSprite;
+        CharacterSprite.SetNativeSize();
+
+        SelectCharacter.SetActive(false);
+        Information.SetActive(true);
+
+
+    }
 
     public void OnCleakJoin()
     {
         if(!(2 < InputField.text.Length && InputField.text.Length <10))
         {
-            Debug.Log("???");
             return;
         }
-
-        playerName.text = InputField.text;
+        GameManager.Instance.SetCharacter(chararcterType, InputField.text, CharacterSprite.sprite);
         Destroy(gameObject);
     }
 }
